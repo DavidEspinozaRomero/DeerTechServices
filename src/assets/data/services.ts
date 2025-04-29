82f46d193a15b871878e2129ff33dc86cc75e56a
David Espinoza <deerhou@gmail.com>
2025-04-29 17:27:11 -0500

    create files services.ts and testimonials.ts for data

---
+++ src/assets/data/services.ts
@@ -0,0 +1,38 @@
+export const services = [
+	{
+		title: "Desarrollo Web",
+		img: "/imgs/frames-design.webp",
+		description:
+			"Interfaces modernas, rápidas y responsivas. Utilizamos IA para automatizar pruebas visuales, validar accesibilidad y sugerir mejoras de UX/UI. Creamos experiencias visuales intuitivas y eficientes que potencian la interacción con tu plataforma.",
+	},
+	{
+		title: "Desarrollo de Software",
+		img: "/imgs/desarrollo-software.webp",
+		description:
+			"Desarrollo de aplicaciones, integración de sistemas. desarrollamos soluciones completas. Aplicamos inteligencia artificial para detectar cuellos de botella, sugerir mejoras estructurales y garantizar que tu sistema sea robusto, escalable y alineado con los objetivos de negocio",
+	},
+	{
+		title: "Desarrollo y Migración de Sistemas Empresariales",
+		img: "/imgs/empresas.webp",
+		description:
+			"Migraciones seguras, adaptamos y optimizamos tus sistemas o construimos nuevos desde cero. Con ayuda de IA, analizamos estructuras existentes, identificamos riesgos y proponemos soluciones eficaces para una migración segura y un rendimiento superior.",
+	},
+	{
+		title: "Consultoría en Seguridad",
+		img: "/imgs/consultor-seguridad.webp",
+		description:
+			"Protege tus datos y los de tus usuarios. Desde la identificación de vulnerabilidades hasta la implementación de soluciones de seguridad, brindamos un servicio personalizado para cada necesidad.",
+	},
+	{
+		title: "Generación de Reportes",
+		img: "/imgs/charts01.webp",
+		description:
+			"Desarrollamos herramientas que convierten tus datos en decisiones. Dashboards y reportes automáticos potenciados con inteligencia artificial para detectar tendencias, generar alertas inteligentes y ayudarte a visualizar tu información financiera con claridad.",
+	},
+	{
+		title: "Consultoría en Tecnología",
+		img: "/imgs/consultor-tech.webp",
+		description:
+			"Desde la identificación de problemas hasta la implementación de soluciones tecnológicas, brindamos un servicio personalizado para cada necesidad. Utilizamos IA para detectar vulnerabilidades y fortalecer la seguridad desde el código.",
+	},
+];
---
+++ src/assets/data/testimonials.ts
@@ -0,0 +1,29 @@
+export const testimonios = [
+	{
+		img: "/imgs/ivi_logo.jpg",
+		title: "Fernando Castillo",
+		subtitle: "CEO | IVI Educa",
+		description: `El desarrollo era exactamente identico al diseño, que en ocaciones me confundia uno con otro. Al inicio le faltaba experiencia, pero con el tiempo mejoró y mejoró mucho. comprometido con su trabajo, puntual, es un hombre recto.`,
+	},
+	{
+		img: "/imgs/vinarytech_logo.jpg",
+		title: "Jimmy Naranjo Coello",
+		subtitle: "Jefe de Desarrollo | Vinary Tech",
+		description:
+			"Durante su tiempo trabajando conmigo, David demostró un alto nivel de profesionalismo y compromiso con su trabajo. Siempre trabajó duro para cumplir con los objetivos y expectativas de su rol siendo un miembro valioso en el equipo.",
+	},
+	{
+		img: "/imgs/lily_logo.png",
+		title: "Lilia Romero",
+		subtitle: "Cliente Independiente | Lily Modistería",
+		description:
+			"El servicio fue muy bueno. Creo una página web que refleja lo que buscaba, y aunque hubo algunos ajustes durante el proceso, siempre mostró disposición para solucionarlos. En general, estoy satisfecha con el resultado.",
+	},
+	{
+		img: "/imgs/blt_logo.webp",
+		title: "Hany Cordon",
+		subtitle: "Co-Founder | Black Label Tech",
+		description:
+			"David ayudo en el mantenimiento y desarrollo de funcionalidades para diferentes entidades financieras, demostrando un alto nivel de ética, profesionalismo y compromiso por el trabajo.",
+	},
+];
