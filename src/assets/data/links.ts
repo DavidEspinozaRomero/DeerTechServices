aacb044ef78ebb9cfac2b00215c42a2706a7e317
David Espinoza <deerhou@gmail.com>
2025-05-08 11:38:21 -0500

    add links.ts: create new file to export various contact links including Calendly, WhatsApp, email, Facebook, and LinkedIn for improved client communication

---
+++ src/assets/data/links.ts
@@ -0,0 +1,5 @@
+export const calendly30min = "https://calendly.com/deerhou/30min";
+export const whatsapp = "https://wa.me/+593939384904?text=Hola!%20Me%20interesa%20tu%20servicio%20conversemos";
+export const email = "mailto:deerhou@gmail.com?subject=Servicios%20TI&body=Hola%20David,\n\nNecesito%20conversar%20tomamos%20un%20café";
+export const facebook = "https://www.facebook.com/people/DEER-Tech-Services/100063722725316/";
+export const linkedin = "https://linkedin.com/in/david-emanuel-espinoza-romero-deerhou";
